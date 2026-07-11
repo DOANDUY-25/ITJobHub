fba332fc345a4e9da73673caceef0a1b280789ef
DOANDUY-25 <doanduy269@gmail.com>
2026-07-11 10:52:00 +0700

    Create project

---
+++ itjobhub/src/main/java/itjobhub/ItjobhubApplication.java
@@ -0,0 +1,13 @@
+package itjobhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItjobhubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItjobhubApplication.class, args);
+	}
+
+}
---
+++ itjobhub/src/test/java/itjobhub/ItjobhubApplicationTests.java
@@ -0,0 +1,13 @@
+package itjobhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItjobhubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
